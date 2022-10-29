722c962faa1b36d4cd3e76185e0a2056a6252948
cmclaughlin24 <curtismclaughlin91@gmail.com>
2022-10-29 10:53:40 -0500

    Fix CSS Name Collisions

--- container/src/App.js
+++ container/src/App.js
@@ -1,16 +1,23 @@
+import { createGenerateClassName, StylesProvider } from '@material-ui/core/styles';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
 
--- marketing/src/App.js
+++ marketing/src/App.js
@@ -1,12 +1,16 @@
-import { StylesProvider } from '@material-ui/core/styles';
+import { createGenerateClassName, StylesProvider } from '@material-ui/core/styles';
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma',
+});
+
 const App = () => {
   return (
-    <StylesProvider>
+    <StylesProvider generateClassName={generateClassName}>
       <BrowserRouter>
         <Switch>
           <Route path="/" exact>
